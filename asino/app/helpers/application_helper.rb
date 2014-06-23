7b97da239306b09bfb26137df0dcee744d23da21
Andrei Beliankou <arbox@yandex.ru>
2014-06-24 00:14:50 +0200

    Completed basic navbar.

--- asino/app/helpers/application_helper.rb
+++ asino/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    if current_page?(link_path)
+      content_tag(:li, :class => 'active') { link_to(link_text, link_path) }
+    else
+      content_tag(:li) { link_to(link_text, link_path) }
+    end
+  end
 end
