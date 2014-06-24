82321b828eea4982b3d47a8cf34e51908e25f620
Andrei Beliankou <arbox@yandex.ru>
2014-06-24 20:53:55 +0200

    Added single user authentication and seeding.

--- asino/app/controllers/static_pages_controller.rb
+++ asino/app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   before_filter :authenticate_user!, :except => [:help, :about]
   def main
+    @documents = Document.all
+    
   end
 
   def help
---
+++ asino/db/seeds/test_user.rb
@@ -0,0 +1,7 @@
+User.create!(
+  {
+    :email => 'testuser@example.com',
+    :password => 'long!_test!_password!',
+    :password_confirmation => 'long!_test!_password!'
+  }
+)
